2e443fb6af8da25af27cffd733cf673f6c3723e9
haran2001 <56040092+haran2001@users.noreply.github.com>
2023-09-07 23:40:12 +0530

    Working base
    working (cleaned up)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,12 @@
-from fastapi import FastAPI, File, UploadFile
-from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
 import numpy as np
 from io import BytesIO
 from PIL import Image
 import tensorflow as tf
 import streamlit as st
-
-# import keras
-# import tensorflow.keras
 import tensorflow as tf
-
-app = FastAPI()
 
 MODEL = tf.keras.models.load_model("model.h5", compile=False)
 CLASS_NAMES = ["Potato___Early_blight", "Potato___Late_blight", "Potato___healthy"]
-
-def ping():
-    return "Hello, I'm alive"
-
-
-def read_file_as_image(data) -> np.ndarray:
-    image = np.array(Image.open(BytesIO(data)))
-    return image
 
 def predict():
     upload = st.file_uploader("Upload your file here...", type=['png', 'jpeg', 'jpg'])
@@ -31,14 +15,8 @@ def predict():
         im = Image.open(upload)
         img = np.asarray(im)
         img_batch = np.expand_dims(img, 0)
-        #image = st.image(uploaded_file)
-    
-    # if image is not None:
-        #image = read_file_as_image(image)
-        #img_batch = np.expand_dims(image, 0)
 
         predictions = MODEL.predict(img_batch)
-        #predictions = MODEL.predict(image)
 
         predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
         confidence = np.max(predictions[0])
@@ -46,10 +24,5 @@ def predict():
     else:
         return {"class": "No Image", "confidence": 1}
     
-
-value = ping()
-st.write('hello world')
-st.write(value)
-predict_output = predict()
 st.write(predict_output['class'])
 

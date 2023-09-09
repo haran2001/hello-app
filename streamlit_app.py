be826707414c3c7074e8519de8ec5ac57ea0d86a
haran2001 <56040092+haran2001@users.noreply.github.com>
2023-09-09 23:19:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,8 +39,8 @@ def predict():
         predictions4 = MODEL4.predict(img_batch)
         
         #Get model predictions for ensemble output
-        all_predictions1 = get_all_predictions(model1, images)
-        all_predictions4 = get_all_predictions(model4, images)
+        all_predictions1 = get_all_predictions(MODEL1, images)
+        all_predictions4 = get_all_predictions(MODEL4, images)
         all_predictions_ensemble = (all_predictions1 + all_predictions4)/2
 
         #Get final prediction
